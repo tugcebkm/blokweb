5152a06d2e629aa1825842932dfc94914e7cdce3
tugcebkm <74149670+tugcebkm@users.noreply.github.com>
2021-01-18 22:43:54 +0100

    Add files via upload

--- read.md
+++ read.md
@@ -14,6 +14,10 @@ Markdown cheat cheet: [Hulp bij het schrijven van Markdown](https://github.com/a
 6.  <https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details>
 7.  <https://www.w3schools.com/howto/howto_js_slideshow.asp>
 8.  <https://www.w3schools.com/html/html_links.asp>
+9. <https://thoughtbot.com/blog/concise-media-queries-with-css-grid>
+10. <https://codepen.io/shooft/pen/ZEpXmrg>
+11. <https://www.youtube.com/watch?v=br-0i3U1VCA>
+12. <https://codepen.io/erikterwan/pen/EVzeRP>
 
 ## Eindgesprek (week 7/8)
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,107 @@
 // JavaScript Document
+function maakProductwrapper(sliderID) {
+	let slider = document.querySelector("#"+sliderID);
+  let sliderElementsContainer = slider.querySelector(":scope > ul");
+	let sliderElements = sliderElementsContainer.querySelectorAll("li");
+  let dots = slider.querySelectorAll(":scope > nav a");
 
-//Slider product pagina //
-//op welke foto hij begint, daar staat de 1 voor
-var slideIndex = 1;
-sliderFoto(slideIndex);
 
-// Slider in werking zetten
-function extraSlide(n) {
-  sliderFoto(slideIndex += n);
+  /****************/
+	/* DE BOLLETJES */
+	/****************/
+
+  // de bolletjes activeren
+  function iniDots() {
+    for (dot of dots) {
+			// elk bolletje laten luisteren naar kliks
+      dots.addEventListener("click", function(e){
+				// als er geklikt wordt
+
+				// de default-actie (de link volgen) niet uitvoeren
+				e.preventDefault();
+
+				// de id van het bijbehorende element bepalen
+				let newElementID = this.hash.substring(1); // de # eraf halen
+
+        // nieuwe element current element maken
+		    updateCurrentElement(newElementID);
+
+				// scrollLeft van de container aanpassen
+		    let theElement = slider.querySelector("#"+newElementID);
+		    let elementOffset = theElement.offsetLeft;
+		    sliderElementsContainer.scrollLeft = elementOffset;
+
+        // de bolletjes updaten
+		    updateDots(newElementID);
+      });
+    }
+	}
+
+
+  /*****************/
+	/* START POSITIE */
+	/*****************/
+
+	// het eerste element en bolletje actief maaken
+  function iniStartPosition() {
+    // eerste element current maken
+    sliderElements[0].classList.add("current");
+    // eerste bolletje current maken
+		dots[0].classList.add("current");
+		// aan het begin van de container starten
+    sliderElementsContainer.scrollLeft = 0;
+  }
+
+
+  /*********************/
+	/* ALGEMENE FUNCTIES */
+	/*********************/
+
+  ////////////////////////////
+	// update current element //
+	function updateCurrentElement(elementID) {
+		// het huidige current element opzoeken
+		let currentElement = slider.querySelector(":scope > ul > .current");
+		// de class current verwijderen
+		currentElement.classList.remove("current");
+
+		// het nieuwe element opzoeken
+		let newElement = slider.querySelector("#"+elementID);
+		// de class current toevoegen
+		newElement.classList.add("current");
+	}
+
+
+  //////////////////////
+  // update dots //
+  function updateDots(elementID){
+		// het huidige current bolletje opzoeken
+		let currentDot = slider.querySelector(":scope > nav .current");
+		// de class current verwijderen
+		currentDot.classList.remove("current");
+
+		// het nieuwe bolletje opzoeken
+    let newDot = slider.querySelector("a[href='#"+elementID+"']");
+		// de class current toevoegen
+		newDot.classList.add("current");
+  }
+
+
+
+	// de bolletjes activeren
+  iniDots();
+  // de slider bij het begin starten
+  iniStartPosition();
 }
 
-// Slider volgorde
-function huidigeSlide(n) {
-  sliderFoto(slideIndex = n);
-}
 
-function sliderFoto(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides-product");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+/************************/
+/* DE slider CREËREN */
+/************************/
+
+// nadat de pagina geladen is, de sliders activeren
+(function() {
+  // hier de id gebruiken van de section in de html
+  maakProductwrapper("slides-Foto");
+  //je kunt hier ook meerdere sliders activeren
+})();
